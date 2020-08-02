b5df2a0a0c8036c2903981cf4b32678cb5a859ff
Rahul Rao <RAOR378647@exactsoftware.com>
2020-08-02 19:40:29 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-reactive-form-app';
+
+  countryList: country[] = [
+    new country("1", "India"),
+    new country('2', 'USA'),
+    new country('3', 'England')
+  ];
+
+  contactForm = new FormGroup({
+    firstname: new FormControl(),
+    lastname: new FormControl(),
+    email: new FormControl(),
+    gender: new FormControl(),
+    isMarried: new FormControl(),
+    country: new FormControl(),
+    address:new FormGroup({
+      city: new FormControl(),
+      street: new FormControl(),
+      pincode:new FormControl()
+    })
+  })
+
+  onSubmit() {
+    console.log(this.contactForm.value);
+  }
+}
+
+
+export class country {
+  id: string;
+  name: string;
+
+  constructor(id: string, name: string) {
+    this.id = id;
+    this.name = name;
+  }
 }
